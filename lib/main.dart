a0dc11723fa1d7709e90d3b3954e63a28d8f323f
Laiba, Azra <laibaazra404@gmail.com>
2026-07-08 14:02:56 -0700

    Updating main.dart file for always opening splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'screens/splash_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
-import 'services/command_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,26 +11,8 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final CommandService _commandService = CommandService();
-
-  @override
-  void initState() {
-    super.initState();
-    // Start listening for commands after app loads
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      Future.delayed(Duration(milliseconds: 500), () {
-        _commandService.listenForCommands(context);
-      });
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,18 +20,8 @@ class _MyAppState extends State<MyApp> {
       title: 'Device Protection',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.purple, fontFamily: 'Poppins'),
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const SplashScreen();
-          }
-          if (snapshot.hasData) {
-            return const HomeScreen();
-          }
-          return const LoginScreen();
-        },
-      ),
+      // ✅ ALWAYS SHOW SPLASH SCREEN FIRST
+      home: const SplashScreen(),
     );
   }
 }
