67403154b53c92f92a8fd064a39a831ea2de1c83
Laiba, Azra <laibaazra404@gmail.com>
2026-09-16 15:03:32 -0700

    Adding app_config.dart in service foldof lib for dual mdoe sandbox and live

--- lib/config/app_config.dart
+++
@@ -1,6 +0,0 @@
-class AppConfig {
-  /// Master switch for your project:
-  /// - Set to [true] when testing with your 3 real clients (Live Production).
-  /// - Set to [false] right before your university viva for instant sandbox demos.
-  static const bool isLiveProduction = true;
-}
---
+++ lib/services/app_config.dart
@@ -0,0 +1,5 @@
+class AppConfig {
+  // Toggle this to TRUE for your 3 real clients (Live Production)
+  // Toggle this to FALSE for your University Viva (Sandbox / Mock Mode)
+  static const bool isLiveProductionMode = false; 
+}
